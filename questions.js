f75d86511f967b21647d6a038fd4ed4a500ea71d
OtpExhaustv2 <sebastien.vanvreckem@gmail.com>
2024-01-28 21:31:27 +0100

    simplification du js + maintenabilité des question

---
+++ questions.js
@@ -0,0 +1,13 @@
+export const questions = [{
+	title: "Peut-on annuler à tout moment?",
+	content: `Oui, il vous suffit de cliquer sur votre <strong> profil > gérer les abonnements > annuler l'abonnement.</strong>`
+}, {
+	title: "Comment rejoindre la communauté ?",
+	content: `Il vous suffit de <a class= "faq__link" href="https://discord.gg/XFP75gW">rejoindre le serveur Discord</a> dans lequel vous pouvez discuter développement web, demander de l'aide ou encore participer à des concours ou des exercices.`
+}, {
+	title: "Qu'est-ce que le parcours Front-End ?",
+	content: `Le <a class="faq__link" href="https://www.ecole-du-web.net/p/parcours">Parcours Front-End</a> est une feuille de route vous permettant de suivre les cours qui feront de vous un développeur Front-End dans l'ordre.`
+}, {
+	title: "Comment s'entraîner ?",
+	content: `Il est très important de pratiquer quand on apprend de nouvelles compétences, c'est pourquoi j'ai <a class="faq__link" href="https://www.ecole-du-web.net/p/entrainement">une page</a> spécialement remplie de dizaines de cours et de projets pratiques!`
+}]
--- script.js
+++ script.js
@@ -1,21 +1,54 @@
-const questionTogglers = document.querySelectorAll(".faq__question-toggler")
-questionTogglers.forEach(toggler => toggler.addEventListener("click", handleQuestionToggle))
+import { questions } from './questions.js';
 
-function handleQuestionToggle(e){
-  const toggler = e.target
-  const contentToToggle = document.getElementById(toggler.getAttribute("aria-controls"))
-  const iconToAnimate = toggler.querySelector(".faq__question-toggler-icon")
-  const toggledState = toggler.getAttribute("aria-expanded") === "true" ? true : false
+const joinClassNames = (...classNames) =>
+	classNames.filter((className) => className).join(' ');
 
+const questionsContainer = document.querySelector('.faq__questions-list');
 
-  if(toggledState) {
-    contentToToggle.classList.remove("faq__content-container--active");
-    toggler.setAttribute("aria-expanded", false)
-    iconToAnimate.classList.remove("faq__question-toggler-icon--active")
-  }
-  else {
-    contentToToggle.classList.add("faq__content-container--active");
-    toggler.setAttribute("aria-expanded", true)
-    iconToAnimate.classList.add("faq__question-toggler-icon--active")
-  }
+questions.forEach((question, i) => {
+	const questionHTML = `
+		<li class="faq__question">
+			<button
+				class="faq__question-toggler"
+				aria-expanded="${i === 0}"
+				aria-controls="content-container-${i}"
+				id="accordion-btn-${i}"
+			>
+				<img
+					src="icons/chevron.svg"
+					alt=""
+					class="${joinClassNames('faq__question-toggler-icon', i === 0 && 'faq__question-toggler-icon--active')}"
+				/>
+				<span class="faq__question-toggler-text">${question.title}<span>
+			</button>
+			<div
+				class="${joinClassNames('faq__content-container', i === 0 && 'faq__content-container--active')}"
+				aria-labelledby="accordion-btn-${i}"
+				id="content-container-${i}"
+			>
+				<div class="faq__content">
+					<p class="faq__content-text">
+						${question.content}
+					</p>
+				</div>
+			</div>
+		</li>
+	`;
+	questionsContainer.innerHTML += questionHTML;
+});
+
+const questionTogglers = document.querySelectorAll('.faq__question-toggler');
+questionTogglers.forEach((toggler) =>
+	toggler.addEventListener('click', handleQuestionToggle)
+);
+
+function handleQuestionToggle(e) {
+	const toggler = e.target;
+	const contentToToggle = toggler.nextElementSibling;
+	const iconToAnimate = toggler.firstElementChild;
+	const toggledState = toggler.getAttribute('aria-expanded') === "true";
+
+	contentToToggle.classList.toggle('faq__content-container--active');
+	toggler.setAttribute('aria-expanded', "" + !toggledState);
+	iconToAnimate.classList.toggle('faq__question-toggler-icon--active');
 }
