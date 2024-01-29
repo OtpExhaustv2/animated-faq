110706744cfe7ee7b8c50e6fd875ff5d6ad9ed74
École du Web <ledesignerduweb@outlook.fr>
2024-01-29 09:00:55 +0100

    simplify JavaScript conditions

--- script.js
+++ script.js
@@ -7,15 +7,9 @@ function handleQuestionToggle(e){
   const iconToAnimate = toggler.querySelector(".faq__question-toggler-icon")
   const toggledState = toggler.getAttribute("aria-expanded") === "true" ? true : false
 
-
-  if(toggledState) {
-    contentToToggle.classList.remove("faq__content-container--active");
-    toggler.setAttribute("aria-expanded", false)
-    iconToAnimate.classList.remove("faq__question-toggler-icon--active")
-  }
-  else {
-    contentToToggle.classList.add("faq__content-container--active");
-    toggler.setAttribute("aria-expanded", true)
-    iconToAnimate.classList.add("faq__question-toggler-icon--active")
-  }
+  
+  const action = toggledState ? 'remove' : 'add';
+  contentToToggle.classList[action]("faq__content-container--active");
+  toggler.setAttribute("aria-expanded", toggledState ? false : true);
+  iconToAnimate.classList[action]("faq__question-toggler-icon--active");
 }
